43cc52c6033053a580717288ae71f35544388736
JinHo <dlwlsghgk124@naver.com>
2022-06-26 15:15:55 +0900

    level4 지형이동 solved

--- level4/지형이동.js
+++ level4/지형이동.js
@@ -1,6 +1,4 @@
-/* 
-
-class DisjointSet {
+class Sets {
     constructor(n) {
         this.roots = Array.from({ length: n }, (_, i) => i);
     }
@@ -16,138 +14,102 @@ class DisjointSet {
     }
 }
 function solution(land, height) {
-    const len = land.length;
-    let visited = Array.from({length:len},
-                               () => Array.from({length:len},() => 0));
-    let area = 1;
-    for(let y = 0;y<len;y++) {
-        for(let x = 0;x<len;x++) {
+    const n = land.length;
+    const visited = Array.from({ length: n }, () =>
+        Array.from({ length: n }, () => -1),
+    );
 
-            if(visited[y][x] === 0) {
-                visited[y][x] = area;
-                dfs(y,x,area,height,visited,land);
+    const dy = [0, 0, 1, -1];
+    const dx = [-1, 1, 0, 0];
+    const inRange = (y, x) => y >= 0 && y < n && x >= 0 && x < n;
+    let area = 0;
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (visited[i][j] === -1) {
+                const stack = [[i, j]];
+                while (stack.length) {
+                    const [y, x] = stack.pop();
+                    visited[y][x] = area;
+                    for (let i = 0; i < 4; i++) {
+                        const nextY = y + dy[i];
+                        const nextX = x + dx[i];
+
+                        if (
+                            inRange(nextY, nextX) &&
+                            visited[nextY][nextX] === -1 &&
+                            Math.abs(land[y][x] - land[nextY][nextX]) <= height
+                        ) {
+                            stack.push([nextY, nextX]);
+                            visited[nextY][nextX] = area;
+                        }
+                    }
+                }
                 area++;
             }
         }
     }
-    
-    
-    const edges = makeEdges(visited,land).sort((a,b) => a[2] - b[2]);
-    
-    const sets = new DisjointSet(len+1);
+
+    const edges = makeEdges(visited, land, area, inRange).sort(
+        (a, b) => a[2] - b[2],
+    );
+
+    const sets = new Sets(area);
+
     let ret = 0;
-    
-    edges.forEach(([a,b,c]) => {
+    edges.forEach(([a, b, c]) => {
         const rootA = sets.find(a);
         const rootB = sets.find(b);
-        if(rootA !== rootB) {
+        if (rootA !== rootB) {
             ret += c;
-            sets.union(a,b);
+            sets.union(a, b);
         }
     });
-    
+    console.log(ret);
     return ret;
 }
-function makeEdges(visited,land) {
-    const dy = [-1,0,1,0];
-    const dx = [0,1,0,-1];
-    const len = land.length;
-    const graph = Array.from({length:len+1},() => 
-                             Array.from({length:len+1},() => Infinity)
-                            );
-    
-    const isntRange = (y,x) => y < 0 || y>=len || x < 0 || x>= len;
-    
-    for(let y = 0;y<len;y++) {
-        for(let x = 0;x<len;x++) {
-            for(let i = 0;i<4;i++) {
+function makeEdges(visited, land, area, inRange) {
+    const n = land.length;
+    const graph = Array.from({ length: area }, () => new Map());
+
+    const dy = [0, 0, -1, 1];
+    const dx = [1, -1, 0, 0];
+
+    for (let y = 0; y < n; y++) {
+        for (let x = y; x < n; x++) {
+            for (let i = 0; i < 4; i++) {
                 const nextY = y + dy[i];
                 const nextX = x + dx[i];
-                if(!isntRange(nextY,nextX)) {
-                       if(visited[y][x] !== visited[nextY][nextX]) {
-                        const a = visited[y][x];
-                        const b = visited[nextY][nextX];
-                        const absDiff = Math.abs(land[y][x] - land[nextY][nextX]);
-                        graph[a][b] = graph[b][a] = Math.min(graph[a][b],absDiff);
-                    }
+                if (
+                    inRange(nextY, nextX) &&
+                    visited[nextY][nextX] !== visited[y][x]
+                ) {
+                    const a = visited[nextY][nextX];
+                    const b = visited[y][x];
+                    const diff = Math.abs(land[nextY][nextX] - land[y][x]);
+                    if (!graph[a].has(b)) graph[a].set(b, Infinity);
+                    graph[a].set(b, Math.min(graph[a].get(b), diff));
                 }
             }
         }
     }
+
     const edges = [];
-    for(let a = 1;a<=len;a++) {
-        for(let b = a;b<=len;b++) {
-            if(graph[a][b] !== Infinity) edges.push([a,b,graph[a][b]]);
+    for (let i = 0; i < area; i++) {
+        if (graph[i].size !== 0) {
+            graph[i].forEach((value, key) => {
+                edges.push([i, key, value]);
+            });
         }
     }
     return edges;
 }
-function dfs(y,x,set,height,visited,land) {
-    const dy = [-1,0,1,0];
-    const dx = [0,1,0,-1];
-    const len = land.length;
-    const isntRange = (y,x) => y < 0 || y >= len || x < 0 || x>=len;
-    
-    for(let i = 0;i<4;i++) {
-        const nextY = y + dy[i];
-        const nextX = x + dx[i];
-        if(!isntRange(nextY,nextX)) {
-               if(visited[nextY][nextX] === 0 &&
-                   Math.abs(land[y][x] - land[nextY][nextX]) <= height ) {
-                    visited[nextY][nextX] = set;
-                    dfs(nextY,nextX,set,height,visited,land);
-            }
-        }
-    }
-    return;
-}
-
-
-*/
-
-function solution(land, height) {
-    let area = 0;
-    const n = land.length;
-    const visited = Array.from({ length: n }, () =>
-        Array.from({ length: n }, () => 0),
-    );
-
-    for (let y = 0; y < n; y++) {
-        for (let x = 0; x < n; x++) {
-            if (visited[y][x] === 0) {
-                visited[y][x] = area;
-                dfs(y, x, area, height, visited, land);
-                area++;
-            }
-        }
-    }
-    console.table(visited);
-}
-function dfs(y, x, set, height, visited, land) {
-    const dy = [-1, 0, 1, 0];
-    const dx = [0, 1, 0, -1];
-    const len = land.length;
-    const isntRange = (y, x) => y < 0 || y >= len || x < 0 || x >= len;
-
-    for (let i = 0; i < 4; i++) {
-        const nextY = y + dy[i];
-        const nextX = x + dx[i];
-        if (!isntRange(nextY, nextX)) {
-            if (
-                visited[nextY][nextX] === 0 &&
-                Math.abs(land[y][x] - land[nextY][nextX]) <= height
-            ) {
-                visited[nextY][nextX] = set;
-                dfs(nextY, nextX, set, height, visited, land);
-            }
-        }
-    }
-    return;
-}
-
-solution([
-    [1, 4, 8, 10],
-    [5, 5, 5, 5],
-    [10, 10, 10, 10],
-    [10, 10, 10, 20],
-]);
+solution(
+    [
+        [1, 4, 8, 10],
+        [5, 5, 5, 5],
+        [10, 10, 10, 10],
+        [10, 10, 10, 20],
+    ],
+    3,
+);
